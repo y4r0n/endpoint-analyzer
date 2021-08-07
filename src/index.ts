2525560ffe700193d9acabfcd493b1b24eaac4bc
Yaron Yarimi <yaron.yarimi@env0.com>
2021-08-07 21:29:28 +0300

    remove redundant console.log

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,4 @@ import { EndpointAnalyzer } from './endpoint-analyzer';
 const analyzer = new EndpointAnalyzer();
 const configFile = process.argv[2] ?? `${process.cwd()}/analyzer.yml`;
 
-analyzer
-    .init(configFile)
-    .then(() => console.log('Finished successfully!'))
-    .catch((err) => console.error(err.toString()));
+analyzer.init(configFile).catch((err) => console.error(err.toString()));
